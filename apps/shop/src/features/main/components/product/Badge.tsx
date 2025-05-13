b3503ff7a3dc1e31a653deefac72a2e298a7b53f
213 <hello-ryan@kakao.com>
2025-05-13 22:13:13 +0900

    feat: 뱃지 UI 수정

--- apps/shop/src/features/main/components/product/Badge.tsx
+++ apps/shop/src/features/main/components/product/Badge.tsx
@@ -1,21 +1,26 @@
 import type { ReactNode } from "react";
 
 interface BadgeProps {
-    variant: "default" | "yellow" | "purple" | "red" | "green";
-    children: ReactNode;
+  variant: "default" | "yellow" | "purple" | "red" | "green";
+  children: ReactNode;
 }
 
 export const Badge = ({ variant, children }: BadgeProps) => {
-    const baseClasses =
-        "inline-block px-1.5 py-1 text-[0.625rem] tracking-tight leading-none text-center rounded-md whitespace-nowrap";
+    
+  const baseClasses =
+    "inline-block px-2.5 py-1.5 text-xs text-center rounded-md whitespace-nowrap";
 
-    const variantClasses = {
-        default: "bg-neutral-700 bg-opacity-20 text-neutral-900",
-        yellow: "bg-yellow-400 text-neutral-900",
-        purple: "bg-purple-800 text-white",
-        red: "bg-red-800 text-white",
-        green: "bg-green-700 text-white",
-    };
+  const variantClasses = {
+    default: "bg-neutral-700 bg-opacity-20 text-neutral-900",
+    yellow: "bg-yellow-400 text-neutral-900",
+    purple: "bg-purple-800 text-white",
+    red: "bg-red-800 text-white",
+    green: "bg-green-700 text-white",
+  };
 
-    return <span className={`${baseClasses} ${variantClasses[variant]} mr-1 mb-1`}>{children}</span>;
+  return (
+    <span className={`${baseClasses} ${variantClasses[variant]} mr-1 mb-1`}>
+      {children}
+    </span>
+  );
 };
