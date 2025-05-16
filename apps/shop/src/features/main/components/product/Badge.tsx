3d9f09e3a1a5788430295c6d9193c024c7a23917
213 <hello-ryan@kakao.com>
2025-05-16 22:47:37 +0900

    feat: cva 적용하여 컴포넌트 작성

--- apps/shop/src/features/main/components/product/Badge.tsx
+++ apps/shop/src/features/main/components/product/Badge.tsx
@@ -1,26 +1,25 @@
 import type { ReactNode } from "react";
+import { cva, type VariantProps } from "@/src/shared/shadcn";
 
-interface BadgeProps {
-  variant: "default" | "yellow" | "purple" | "red" | "green";
-  children: ReactNode;
+const badgeVariants = cva("inline-block px-2.5 py-1.5 text-xs text-center rounded-md whitespace-nowrap mr-1 mb-1", {
+    variants: {
+        variant: {
+            default: "bg-neutral-700 bg-opacity-20 text-neutral-900",
+            yellow: "bg-yellow-400 text-neutral-900",
+            purple: "bg-purple-800 text-white",
+            red: "bg-red-800 text-white",
+            green: "bg-green-700 text-white",
+        },
+    },
+    defaultVariants: {
+        variant: "default",
+    },
+});
+
+interface BadgeProps extends VariantProps<typeof badgeVariants> {
+    children: ReactNode;
 }
 
-export const Badge = ({ variant, children }: BadgeProps) => {
-    
-  const baseClasses =
-    "inline-block px-2.5 py-1.5 text-xs text-center rounded-md whitespace-nowrap";
-
-  const variantClasses = {
-    default: "bg-neutral-700 bg-opacity-20 text-neutral-900",
-    yellow: "bg-yellow-400 text-neutral-900",
-    purple: "bg-purple-800 text-white",
-    red: "bg-red-800 text-white",
-    green: "bg-green-700 text-white",
-  };
-
-  return (
-    <span className={`${baseClasses} ${variantClasses[variant]} mr-1 mb-1`}>
-      {children}
-    </span>
-  );
+export const Badge = ({ variant = "default", children }: BadgeProps) => {
+    return <span className={badgeVariants({ variant })}>{children}</span>;
 };
