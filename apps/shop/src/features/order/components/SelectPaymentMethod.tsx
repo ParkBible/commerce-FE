10d5704139feadbf95db1d9edcb022a51d11d437
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2025-05-20 17:20:05 +0900

    feat: 결제 수단 선택 UI

---
+++ apps/shop/src/features/order/components/SelectPaymentMethod.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface SelectPaymentMethodProps {
+    onChange: (paymentMethod: string) => void;
+}
+
+export default function SelectPaymentMethod({ onChange }: SelectPaymentMethodProps) {
+    const [paymentMethod, setPaymentMethod] = useState<string>("toss");
+
+    const handlePaymentMethodChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPaymentMethod(e.target.value);
+    };
+
+    useEffect(() => {
+        onChange(paymentMethod);
+    }, [paymentMethod, onChange]);
+
+    return (
+        <div>
+            <div className="flex items-center gap-2">
+                <input type="radio" name="payment-method" id="toss" onChange={handlePaymentMethodChange} />
+                <label htmlFor="toss">토스페이</label>
+            </div>
+            <div className="flex items-center gap-2">
+                <input type="radio" name="payment-method" id="credit-card" onChange={handlePaymentMethodChange} />
+                <label htmlFor="credit-card">신용카드 결제</label>
+            </div>
+            <div className="flex items-center gap-2">
+                <input type="radio" name="payment-method" id="bank-transfer" onChange={handlePaymentMethodChange} />
+                <label htmlFor="bank-transfer">계좌이체</label>
+            </div>
+        </div>
+    );
+}
