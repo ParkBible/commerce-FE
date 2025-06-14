5815f9011c09f46e43234667eaf95d4f21e1e951
213 <hello-ryan@kakao.com>
2025-06-14 09:14:12 +0900

    feat: 메인에 스크램블 효과 추가 (#80)
    * feat: 메인에 스크램블 효과 추가
    
    * fix: 스크램블 범위를 한글 유니코드로 변경

--- apps/shop/src/features/main/components/HeroBanner.tsx
+++ apps/shop/src/features/main/components/HeroBanner.tsx
@@ -1,4 +1,21 @@
+"use client";
+
+import { useScramble } from "use-scramble";
+
 function HeroBanner() {
+    const { ref } = useScramble({
+        text: "바코드 브루잉",
+        range: [44032, 55203],
+        speed: 1,
+        tick: 1,
+        step: 10,
+        scramble: 25,
+        seed: 3,
+        chance: 1,
+        overdrive: false,
+        overflow: false,
+    });
+
     return (
         <section className="relative w-full">
             <img
@@ -11,7 +28,9 @@ function HeroBanner() {
                 <div className="w-full px-6 py-20">
                     <div className="mx-auto text-center">
                         <h1 className="text-6xl font-bold tracking-tight leading-[4.5rem] text-white max-md:text-4xl">801 버츄오 커피</h1>
-                        <p className="mt-6 text-lg font-semibold leading-6 text-white">혁신적인 바코드 브루잉으로 열리는 차원이 다른 커피</p>
+                        <p className="mt-6 text-lg font-semibold leading-6 text-white">
+                            혁신적인 <span ref={ref} />으로 열리는 차원이 다른 커피
+                        </p>
                     </div>
                 </div>
             </div>
