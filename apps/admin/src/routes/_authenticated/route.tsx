66ac474aadf7c91a9d76c27dce9f98cbf63b7686
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2025-06-21 02:52:34 +0900

    Feat/admin (#159)
    * fix : 고객 관리, 대시보드 메뉴 삭제
    
    * fix : 인증 주석 처리 수정
    
    ---------
    
    Co-authored-by: hbs9312 <hbs30832@naver.com>

--- apps/admin/src/routes/_authenticated/route.tsx
+++ apps/admin/src/routes/_authenticated/route.tsx
@@ -1,28 +1,28 @@
 import RootLayout from "@/features/common/layout/RootLayout";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 // 인증 상태 확인 함수
-// const isAuthenticated = () => {
-//     try {
-//         const storage = localStorage.getItem("admin-auth-storage");
-//         if (!storage) return false;
+const isAuthenticated = () => {
+    try {
+        const storage = localStorage.getItem("admin-auth-storage");
+        if (!storage) return false;
 
-//         const parsed = JSON.parse(storage);
-//         return parsed?.state?.isAuthenticated === true && parsed?.state?.tokens?.accessToken;
-//     } catch (error) {
-//         console.error("인증 상태 확인 오류:", error);
-//         return false;
-//     }
-// };
+        const parsed = JSON.parse(storage);
+        return parsed?.state?.isAuthenticated === true && parsed?.state?.tokens?.accessToken;
+    } catch (error) {
+        console.error("인증 상태 확인 오류:", error);
+        return false;
+    }
+};
 
 export const Route = createFileRoute("/_authenticated")({
     component: RootLayout,
     beforeLoad: () => {
         // 로그인되지 않은 경우 로그인 페이지로 리디렉션
-        // if (!isAuthenticated()) {
-        //     throw redirect({
-        //         to: "/login",
-        //     });
-        // }
+        if (!isAuthenticated()) {
+            throw redirect({
+                to: "/login",
+            });
+        }
     },
 });
